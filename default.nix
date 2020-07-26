437b13ca258098352c038b875ccb4ac0ba1f9088
Farid Zakaria <fmzakari@google.com>
2020-07-25 20:41:49 -0700

    Fix nix-shell

--- default.nix
+++ default.nix
@@ -2,11 +2,14 @@ let
   moz_overlay = import (builtins.fetchTarball
     "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override {
-    extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];
-  });
+  rust-stable = nixpkgs.latest.rustChannels.stable.rust.override {
+    extensions = [ "rust-src" "rust-analysis" "rustfmt-preview" "rls-preview"];
+  };
 in with nixpkgs;
 mkShell {
   name = "scriptr";
-  buildInputs = [ ruststable ];
+  buildInputs = [ rust-stable rustracer evcxr ];
+  RUST_BACKTRACE = 0;
+  RUST_SRC_PATH = "${rust-stable}/lib/rustlib/src/rust/src";
+
 }
