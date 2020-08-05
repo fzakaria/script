e94d6c8091c3c33c14e3a77071ab27d9b141fbb3
Farid Zakaria <farid.m.zakaria@gmail.com>
2020-08-05 12:11:06 -0700

    Added small part

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
 let
   pkgs =
     import <nixpkgs> {};
+  rust-toolchain = pkgs.symlinkJoin {
+    name = "rust-toolchain";
+    paths = [pkgs.rustc pkgs.cargo pkgs.rustPlatform.rustcSrc];
+  };
 in with pkgs;
 mkShell {
   name = "scriptr";
-  buildInputs = [rustc cargo evcxr rustracer];
+  buildInputs = [rust-toolchain evcxr rustracer];
   RUST_BACKTRACE = 1;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
 extern crate pty;
 extern crate nix;
+extern crate libc;
 
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate simple_error;
+
+use std::os::unix::io::AsRawFd;
+use simple_error::SimpleError;
+use std::io::Stdin;
+
+fn main() -> Result<(), Box<dyn std::error::Error> >  {
+    // 1 -- First get the original terminal attributes
+    let stdin = std::io::stdin();
+    let orig_attr = nix::sys::termios::tcgetattr(stdin.as_raw_fd())?;
+
+    unsafe {
+        let window = get_window(stdin)?;
+        println!("{:?}", window)
+    }
+
+    Ok(())
 }
+
+unsafe fn get_window(stdin: Stdin) -> Result<libc::winsize, SimpleError> {
+    let mut window: std::mem::MaybeUninit<libc::winsize> = std::mem::MaybeUninit::uninit();
+    let result = libc::ioctl(stdin.as_raw_fd(), libc::TIOCGWINSZ, window.as_mut_ptr());
+    if result < 0 {
+        bail!("Failed to get window size.");
+    }
+    Ok(window.assume_init())
+}
+
